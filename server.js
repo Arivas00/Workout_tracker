8d472a9e5a41259beb8f766ed164d089d4d42831
Alex <a.chisar00@gmail.com>
2021-06-10 18:45:59 -0400

    File structure setup

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
